3e8b977ed94b99be9ddf87aa45044efa07d18bda
Kshitij Srivastava <kshitijsrivastava2312@gmail.com>
2022-03-09 13:45:47 +0530

    Changed searching of medicines to name rather than ID in forms.

--- static/scripts/currentMedicine.js
+++ static/scripts/currentMedicine.js
@@ -5,15 +5,39 @@
 /**
  * Get the currently selected medicine from its ID by matching against all medicines in the database.
  *
- * @param medicineData Data of all medicines in the database as JavaScript objects.
+ * @param medicineData Data of all medicines in the database a JavaScript object.
  * @return {*} Current medicine being selected.
  */
 function getCurrentMedicine(medicineData) {
-    const selectedID = parseInt(document.getElementById('medicine-id').value);
+    const selectedName = document.getElementById('medicine-name').value;
 
     for (let medicine of medicineData) {
-        if (medicine.id === selectedID) {
+        if (medicine.name === selectedName) {
             return medicine
         }
     }
+}
+
+/**
+ * Get the matching medicine from the search bar by checking it against all medicine names in the database.
+ *
+ * @param medicineData Data of all medicines in the database as a JavaScript object.
+ * @return {*[]} Array of all matching medicines.
+ */
+function getValidMedicines(medicineData) {
+    const medicineName = document.getElementById('search-bar').value.toLowerCase();
+    const validMedicines = [];
+
+    // Show the first five medicines if search bar is empty.
+    if (medicineName === '') {
+        return medicineData.slice(0, 5);
+    }
+
+    for (let medicine of medicineData) {
+        if (medicine.name.toLowerCase().includes(medicineName)) {
+            validMedicines.push(medicine);
+        }
+    }
+
+    return validMedicines.slice(0, 8);
 }
